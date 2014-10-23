a052ec50a204ae5cf3459ed7526b3ab69ff9e349
alumno <alumno@2A5PC16.fpmislata.local>
2014-10-23 08:55:28 +0200

    mejoraBaseDatos

---
+++ src/basedatosborrar/BaseDatos.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package basedatosborrar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alumno
+ */
+public class BaseDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        
+        //CREACION DEL OBJETO PARA LLAMAR A LAS FUNCIONES DE LA CLASE 'EntidadBancaria'
+            EntidadBancaria entidadBancaria = new EntidadBancaria();
+            entidadBancaria.setIdEntidadBancaria(1);
+            entidadBancaria.setNombre("caixaBank");
+            entidadBancaria.setCodigoEntidad("00" + entidadBancaria.getIdEntidadBancaria());
+            entidadBancaria.setFechaCreacion(null);
+        
+        //CREACIÓN DEL OBJETO PARA LLAMAR A LAS FUNCIONES DE LA CLASE 'EntidadBancariaDAO'
+            EntidadBancariaDAO entidadBancariaDAO = new EntidadBancariaDAO();
+        
+        //BORRAR UNA FILA CONCRETA DE LA TABLA 'entidadBancaria'
+//            entidadBancariaDAO.delete(4); 
+        
+        //INSERTAR UNA FILA CONCRETA DE LA TABLA 'entidadBancaria'
+            entidadBancariaDAO.insert(entidadBancaria);
+        
+        //MODIFICAR UNA FILA CONCRETA DE LA TABLA 'entidadBancaria'
+//            entidadBancariaDAO.update(3);
+         
+        
+    }   
+}
---
+++ src/basedatosborrar/EntidadBancariaDAO.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package basedatosborrar;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author alumno
+ */
+public class EntidadBancariaDAO {
+    public void delete (int idEntidadBancaria) throws SQLException {
+         
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_bancario","root", "root");
+        String deleteTableSQL = "DELETE FROM entidadbancaria WHERE idEntidadBancaria = ?";
+        
+        PreparedStatement preparedStatement = conn.prepareStatement(deleteTableSQL);
+        preparedStatement.setInt(1, idEntidadBancaria);
+        
+        //EJECUTAR LA CONSULTA DELETE
+        preparedStatement.executeUpdate();
+    }    
+    
+    public void insert(EntidadBancaria entidadBancaria) throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_bancario","root", "root");
+        String insertTableSQL = "INSERT INTO entidadbancaria VALUES (?, ?, ?, ?)";
+        
+        PreparedStatement preparedStatement = conn.prepareStatement(insertTableSQL);
+        preparedStatement.setInt(1, entidadBancaria.getIdEntidadBancaria());
+        preparedStatement.setString(2, entidadBancaria.getNombre());
+        preparedStatement.setString(3, entidadBancaria.getCodigoEntidad());
+        preparedStatement.setTimestamp(4, getCurrentTimeStamp());
+        
+        //EJECUTAR LA CONSULTA INSERT
+        preparedStatement.executeUpdate();
+    }
+    
+    public void update(int idEntidadBancaria) throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_bancario","root", "root");
+        String updateTableSQL = "UPDATE entidadbancaria SET nombre = ? WHERE idEntidadBancaria = ?";
+        
+        PreparedStatement preparedStatement = conn.prepareStatement(updateTableSQL);
+        preparedStatement.setString(1, "caixaBank");
+        preparedStatement.setInt(2, idEntidadBancaria);
+        
+        //EJECUTAR LA CONSULTA UPDATE
+        preparedStatement.executeUpdate();
+    }
+    
+    private static java.sql.Timestamp getCurrentTimeStamp() {
+ 
+        java.util.Date today = new java.util.Date();
+        return new java.sql.Timestamp(today.getTime());
+ 
+    }
+    
+}
